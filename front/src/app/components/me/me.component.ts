bf3b7fa9b9d748f3ec87f6e9cf40cede6ebe5849
Samuel Bart <bartsamuel@me.com>
2026-07-29 12:21:35 +0200

    refactor(me-component): add takeUntilDestroyed for subscribe and remplace template control flow

--- front/src/app/components/me/me.component.ts
+++ front/src/app/components/me/me.component.ts
@@ -1,30 +1,32 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { User } from '../../core/models/user.interface';
 import { SessionService } from '../../core/service/session.service';
 import { UserService } from '../../core/service/user.service';
-import { MaterialModule } from "../../shared/material.module";
-import { CommonModule } from "@angular/common";
+import { MaterialModule } from '../../shared/material.module';
 
 @Component({
   selector: 'app-me',
   imports: [CommonModule, MaterialModule],
   templateUrl: './me.component.html',
-  styleUrls: ['./me.component.scss']
+  styleUrls: ['./me.component.scss'],
 })
 export class MeComponent implements OnInit {
   private router = inject(Router);
   private sessionService = inject(SessionService);
   private matSnackBar = inject(MatSnackBar);
   private userService = inject(UserService);
+  private destroyRef = inject(DestroyRef);
   public user: User | undefined;
-
 
   ngOnInit(): void {
     this.userService
       .getById(this.sessionService.sessionInformation!.id.toString())
-      .subscribe((user: User) => this.user = user);
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((user: User) => (this.user = user));
   }
 
   public back(): void {
@@ -34,11 +36,13 @@ export class MeComponent implements OnInit {
   public delete(): void {
     this.userService
       .delete(this.sessionService.sessionInformation!.id.toString())
-      .subscribe((_) => {
-        this.matSnackBar.open("Your account has been deleted !", 'Close', { duration: 3000 });
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => {
+        this.matSnackBar.open('Your account has been deleted !', 'Close', {
+          duration: 3000,
+        });
         this.sessionService.logOut();
         this.router.navigate(['/']);
-      })
+      });
   }
-
 }
