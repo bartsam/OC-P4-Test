51e32ad690fbba61e9ed776855caa337bfdcf74e
Samuel Bart <bartsamuel@me.com>
2026-07-30 11:03:37 +0200

    refactor(user): remove try/catch add global exception handling and move logic to UserService

--- back/src/main/java/com/openclassrooms/starterjwt/services/UserService.java
+++ back/src/main/java/com/openclassrooms/starterjwt/services/UserService.java
@@ -1,22 +1,51 @@
 package com.openclassrooms.starterjwt.services;
 
+import com.openclassrooms.starterjwt.dto.UserDto;
+import com.openclassrooms.starterjwt.exception.NotFoundException;
+import com.openclassrooms.starterjwt.exception.UnauthorizedException;
+import com.openclassrooms.starterjwt.mapper.UserMapper;
 import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.repository.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,  UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
-    public void delete(Long id) {
-        this.userRepository.deleteById(id);
+    private User findById(Long id) {
+        User user = this.userRepository.findById(id).orElse(null);
+
+        if (user == null) {
+            throw new NotFoundException();
+        }
+
+        return user;
     }
 
-    public User findById(Long id) {
-        return this.userRepository.findById(id).orElse(null);
+    public UserDto findDtoById(Long id) {
+        return this.userMapper.toDto(findById(id));
+    }
+
+    public void deleteIfOwner(Long id) {
+        User user = findById(id);
+
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+
+        if (!Objects.equals(userDetails.getUsername(), user.getEmail())) {
+            throw new UnauthorizedException();
+        }
+
+        userRepository.deleteById(id);
     }
 }
