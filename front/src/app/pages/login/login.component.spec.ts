f2e340b028caf7cd4c226239b9397c1a5fa0a035
Samuel Bart <bartsamuel@me.com>
2026-08-10 15:47:33 +0200

    refactor(test): remove unused imports and disable animations

--- front/src/app/pages/login/login.component.spec.ts
+++ front/src/app/pages/login/login.component.spec.ts
@@ -1,14 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect, jest } from '@jest/globals';
 
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, Router } from '@angular/router';
 import { of, throwError } from 'rxjs';
 import { SessionInformation } from '../../core/models/sessionInformation.interface';
@@ -42,19 +37,12 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [LoginComponent],
       providers: [
         provideRouter([]),
+        provideNoopAnimations(),
         { provide: AuthService, useValue: mockAuthService },
         { provide: SessionService, useValue: mockSessionService },
-      ],
-      imports: [
-        LoginComponent,
-        BrowserAnimationsModule,
-        MatCardModule,
-        MatIconModule,
-        MatFormFieldModule,
-        MatInputModule,
-        ReactiveFormsModule,
       ],
     }).compileComponents();
 
@@ -75,7 +63,7 @@ describe('LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should log the user in and navigate to /sessions when the login succeeds', () => {
+  it('should log in the user and navigate to "/sessions" when succeeds', () => {
     mockAuthService.login.mockReturnValue(of(mockSessionInformation));
 
     component.form.setValue({
@@ -127,16 +115,5 @@ describe('LoginComponent', () => {
   it('should disable the submit button when the form is empty', () => {
     const submitButton = debugElement.query(By.css('button[type="submit"]'));
     expect(submitButton.nativeElement.disabled).toBe(true);
-  });
-
-  it('should enable the submit button when the form is valid', () => {
-    component.form.setValue({
-      email: 'john.doe@test.com',
-      password: 'azerty',
-    });
-    fixture.detectChanges();
-
-    const submitButton = debugElement.query(By.css('button[type="submit"]'));
-    expect(submitButton.nativeElement.disabled).toBe(false);
   });
 });
