9a33404f30ae386010f6c5f4dc2e3049ceb1689a
Samuel Bart <bartsamuel@me.com>
2026-08-12 15:34:14 +0200

    test(e2e) add login e2e test

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,28 +1,65 @@
+import { faker } from '@faker-js/faker';
+
 describe('Login spec', () => {
-  it('Login successfull', () => {
+  beforeEach(() => {
     cy.visit('/login');
+  });
 
-    cy.intercept('POST', '/api/auth/login', {
-      body: {
-        id: 1,
-        username: 'userName',
-        firstName: 'firstName',
-        lastName: 'lastName',
-        admin: true,
-      },
-    });
-
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/api/session',
-      },
-      [],
-    ).as('session');
+  it('should login admin successfuly', () => {
+    cy.intercept('POST', '/api/auth/login').as('login');
+    cy.intercept('GET', '/api/session').as('session');
 
     cy.getByTestId('email-input').type('yoga@studio.com');
-    cy.getByTestId('password-input').type(`${'test!1234'}{enter}{enter}`);
+    cy.getByTestId('password-input').type(`${'test!1234'}{enter}`);
 
+    cy.wait('@login').its('response.statusCode').should('eq', 200);
+    cy.wait('@session').its('response.statusCode').should('eq', 200);
     cy.url().should('include', '/sessions');
+  });
+
+  it('should login user successfully', () => {
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 200,
+      body: {
+        id: faker.number.int(),
+        username: faker.internet.email(),
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        admin: false,
+      },
+    }).as('login');
+    cy.intercept('GET', '/api/session', []).as('session');
+
+    cy.getByTestId('email-input').type(faker.internet.email());
+    cy.getByTestId('password-input').type(
+      `${faker.internet.password({ length: 10 })}{enter}`,
+    );
+
+    cy.wait('@login');
+    cy.wait('@session');
+    cy.url().should('include', '/sessions');
+  });
+
+  it('should fail login with wrong credentials', () => {
+    cy.intercept('POST', '/api/auth/login').as('login');
+
+    cy.getByTestId('email-input').type('wrong@studio.com');
+    cy.getByTestId('password-input').type('wrongPassword');
+    cy.getByTestId('submit-button').click();
+
+    cy.wait('@login').its('response.statusCode').should('eq', 401);
+    cy.getByTestId('error-message').should('be.visible');
+    cy.url().should('include', '/login');
+  });
+
+  it('should displays required field error when submitting', () => {
+    cy.getByTestId('submit-button').should('be.disabled');
+
+    cy.getByTestId('email-input').type('yoga@studio.com');
+    cy.getByTestId('submit-button').should('be.disabled');
+
+    cy.getByTestId('email-input').clear();
+    cy.getByTestId('password-input').type('test!1234');
+    cy.getByTestId('submit-button').should('be.disabled');
   });
 });
