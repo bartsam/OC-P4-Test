99493215c74af55224eb9c4bc0d461b315a3fc15
Samuel Bart <bartsamuel@me.com>
2026-08-14 18:05:14 +0200

    test(teacher): add unit test on service

--- back/src/main/java/com/openclassrooms/starterjwt/controllers/TeacherController.java
+++ back/src/main/java/com/openclassrooms/starterjwt/controllers/TeacherController.java
@@ -1,6 +1,8 @@
 package com.openclassrooms.starterjwt.controllers;
 
 import com.openclassrooms.starterjwt.dto.TeacherDto;
+import com.openclassrooms.starterjwt.mapper.TeacherMapper;
+import com.openclassrooms.starterjwt.models.Teacher;
 import com.openclassrooms.starterjwt.services.TeacherService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,19 +16,23 @@ import java.util.List;
 @RequestMapping("/api/teacher")
 public class TeacherController {
     private final TeacherService teacherService;
+    private final TeacherMapper teacherMapper;
 
 
-    public TeacherController(TeacherService teacherService) {
+    public TeacherController(TeacherService teacherService, TeacherMapper teacherMapper) {
         this.teacherService = teacherService;
+        this.teacherMapper = teacherMapper;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TeacherDto> findById(@PathVariable("id") String id) {
-        return ResponseEntity.ok().body(this.teacherService.findDtoById(Long.valueOf(id)));
+        Teacher teacher = teacherService.findById(Long.valueOf(id));
+        return ResponseEntity.ok(teacherMapper.toDto(teacher));
     }
 
     @GetMapping()
     public ResponseEntity<List<TeacherDto>> findAll() {
-        return ResponseEntity.ok().body(this.teacherService.findAllDto());
+        List<Teacher> teachers = teacherService.findAll();
+        return ResponseEntity.ok().body(teacherMapper.toDto(teachers));
     }
 }
