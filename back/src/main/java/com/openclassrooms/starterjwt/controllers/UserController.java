78b33c91dc0e2464a29a1d8aeeeff8246d14b818
Samuel Bart <bartsamuel@me.com>
2026-08-15 10:18:24 +0200

    test(user): add unit test on service

--- back/src/main/java/com/openclassrooms/starterjwt/controllers/UserController.java
+++ back/src/main/java/com/openclassrooms/starterjwt/controllers/UserController.java
@@ -1,28 +1,28 @@
 package com.openclassrooms.starterjwt.controllers;
 
 import com.openclassrooms.starterjwt.dto.UserDto;
+import com.openclassrooms.starterjwt.mapper.UserMapper;
+import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
+    private final UserMapper userMapper;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserMapper userMapper) {
         this.userService = userService;
+        this.userMapper = userMapper;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> findById(@PathVariable("id") String id) {
-        UserDto userDto = this.userService.findDtoById(Long.valueOf(id));
-        return ResponseEntity.ok().body(userDto);
+        User user = this.userService.findById(Long.valueOf(id));
+        return ResponseEntity.ok().body(userMapper.toDto(user));
     }
 
     @DeleteMapping("{id}")
