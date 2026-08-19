38a1ca1263c43247dc95324da64e6219167d739c
Samuel Bart <bartsamuel@me.com>
2026-08-19 19:04:23 +0200

    refactor(cypress): factorize login commands to reduce duplication

--- front/cypress/support/commands.ts
+++ front/cypress/support/commands.ts
@@ -1,47 +1,3 @@
-// ***********************************************
-// This example namespace declaration will help
-// with Intellisense and code completion in your
-// IDE or Text Editor.
-// ***********************************************
-// declare namespace Cypress {
-//   interface Chainable<Subject = any> {
-//     customCommand(param: any): typeof customCommand;
-//   }
-// }
-//
-// function customCommand(param: any): void {
-//   console.warn(param);
-// }
-//
-// NOTE: You can use it like so:
-// Cypress.Commands.add('customCommand', customCommand);
-//
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
 declare namespace Cypress {
   interface Chainable<Subject = any> {
     /**
@@ -68,16 +24,18 @@ Cypress.Commands.add('getByTestId', (id: string) => {
   return cy.get(`[data-testid=${id}]`);
 });
 
-Cypress.Commands.add('loginAsAdmin', () => {
+function login(email: string, password: string, sessionName: string) {
   cy.session(
-    'adminSession',
+    sessionName,
     () => {
-      cy.request('POST', '/api/auth/login', {
-        email: 'yoga@studio.com',
-        password: 'test!1234',
-      }).then(({ body }) => {
-        window.localStorage.setItem('sessionInformation', JSON.stringify(body));
-      });
+      cy.request('POST', '/api/auth/login', { email, password }).then(
+        ({ body }) => {
+          window.localStorage.setItem(
+            'sessionInformation',
+            JSON.stringify(body),
+          );
+        },
+      );
     },
     {
       validate() {
@@ -86,27 +44,14 @@ Cypress.Commands.add('loginAsAdmin', () => {
     },
   );
   cy.visit('/sessions');
-});
+}
 
-Cypress.Commands.add('loginAsUser', () => {
-  cy.session(
-    'userSession',
-    () => {
-      cy.request('POST', '/api/auth/login', {
-        email: 'user@studio.com',
-        password: 'test!1234',
-      }).then(({ body }) => {
-        window.localStorage.setItem('sessionInformation', JSON.stringify(body));
-      });
-    },
-    {
-      validate() {
-        expect(localStorage.getItem('sessionInformation')).to.exist;
-      },
-    },
-  );
-  cy.visit('/sessions');
-});
+Cypress.Commands.add('loginAsAdmin', () =>
+  login('yoga@studio.com', 'test!1234', 'adminSession'),
+);
+Cypress.Commands.add('loginAsUser', () =>
+  login('user@studio.com', 'test!1234', 'userSession'),
+);
 
 Cypress.Commands.add('getSessionToken', () => {
   return cy.window().then((win) => {
